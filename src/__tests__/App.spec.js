586fe65ea82731ead8b81a740676e56a03237c92
burcusevinc <sburcusevinc@gmail.com>
2022-01-24 03:24:30 +0300

    assignment-3 final

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -9,7 +9,7 @@ describe("App.vue", () => {
         it('sanity test', () => {
             return
         })
-        //0.sanity check
+        //0.app component check
         it("should component exists", () => {
             const wrapper = mountComponent()
             expect(wrapper.exists()).toBeTruthy()
@@ -17,37 +17,50 @@ describe("App.vue", () => {
         //1.h1 exist check
         it("should h1 title exists", () => {
             const wrapper = mountComponent()
-            const title = wrapper.find("#title")
-            expect(title.exists()).toBeTruthy()
-        }) //2. h1 text check
-        it("should h1 title text exist and match", () => {
+            const title = wrapper.find("#title") //h1 element find
+            expect(title.exists()).toBeTruthy() //exist
+        }) 
+        //2. h1 text matching text
+        it("should h1 title text match", () => {
             const wrapper = mountComponent()
+            //h1 element's text should equal to "Daily Corona Cases in Turkey"
             expect(wrapper.find("#title").text()).toEqual("Daily Corona Cases in Turkey")
         })
     })
 
     describe('check notificationArea', () => {
-        //3.notificationArea class check based on getCount Value
+        //3.notificationArea classes checks based on getCount Value
         it("should notificationArea class work correctly", async () => {
+            let countVar = 0
+            let localThis =  {
+                $store: {
+                    state: {
+                        count: countVar
+                    }
+                }
+            }
             let wrapper = mountComponent()
 
-            state.count = 0
-            const safeClass = wrapper.find(".notificationArea")
-            expect(safeClass.classes()).toContain("safe")
+            countVar = 0
+            wrapper.vm.$store.state.count = countVar
+            const safeClass = wrapper.find(".notificationArea") //find the class
+            expect(safeClass.classes()).toContain("safe")  // if countVar is 0, class should be "safe"
 
-            state.count = 6
-            //DOM'un güncellenmesi bekletilir.DOM güncellenmezse, "safe" olarak kalacaktı.
+            countVar = 5
+            wrapper.vm.$store.state.count = countVar
+            //Waits for the DOM to update. If the DOM doesn't update, the class will be "safe"
             await wrapper.vm.$nextTick() 
             const NormalClass = wrapper.find(".notificationArea")
-            expect(NormalClass.classes()).toContain("normal")
+            expect(NormalClass.classes()).toContain("normal") // if countVar is 5, class should be "normal"
 
-            state.count = 11
+            countVar = 10
+            wrapper.vm.$store.state.count = countVar
             await wrapper.vm.$nextTick()
             const DangerClass = wrapper.find(".notificationArea")
-            expect(DangerClass.classes()).toContain("danger")
+            expect(DangerClass.classes()).toContain("danger")//if countVar is 10, class should be "danger"
         })
 
-        //4.notificationArea text message check
+        //4. text messages check of notificationArea classes 
         it("should text message show correctly", async () => {
             let wrapper = mountComponent()
             let countVar = 0
@@ -58,25 +71,31 @@ describe("App.vue", () => {
                     }
                 }
             }
-            let message = App.computed.message.call(localThis)
+            let message = App.computed.message.call(localThis) 
             wrapper.vm.$store.state.count = countVar      
             await wrapper.vm.$nextTick()
+
+            //if countVar is 0, class text should be equal to the string below.
             expect(wrapper.find(".notificationArea").text()).toEqual(`So safe. Case count is ${countVar}k`)
 
-            countVar = 6
+            countVar = 5
             wrapper.vm.$store.state.count = countVar      
             await wrapper.vm.$nextTick()
+
+            //if countVar is 5, class text should be equal to the string below.
             expect(wrapper.find(".notificationArea").text()).toEqual(`Life is normal. Case count is ${countVar}k`)
 
-            countVar = 11
+            countVar = 10
             wrapper.vm.$store.state.count = countVar      
             await wrapper.vm.$nextTick()
+
+            //if countVar is 10, class text should be equal to the string below.
             expect(wrapper.find(".notificationArea").text()).toEqual(`Danger!!! Case count is ${countVar}k`)
         })
     })
 })
 
-//mounts the component + create local vue and store.
+//mounts the component + created the local vue and store.
 function mountComponent() {
     const localVue = createLocalVue()
     localVue.use(Vuex)
@@ -84,7 +103,8 @@ function mountComponent() {
     return mount(App, {
         localVue,
         store: new Vuex.Store({
-            state,
+            //we must use stringify and parse methods because we don't want to manipulate the real state object.
+            state : JSON.parse(JSON.stringify(state)), 
             getters,
             mutations,
             actions
--- src/__tests__/Counter.spec.js
+++ src/__tests__/Counter.spec.js
@@ -3,7 +3,7 @@ import Counter from "@/Counter"
 import Vuex from "vuex";
 import { actions, state, getters, mutations } from "@/store";
 
-//mounts the component + create local vue and store.
+//mounts the component + created the local vue and store.
 function mountComponent() {
     const localVue = createLocalVue()
     localVue.use(Vuex)
@@ -11,7 +11,8 @@ function mountComponent() {
     return shallowMount(Counter, {
         localVue,
         store: new Vuex.Store({
-            state : JSON.parse(JSON.stringify(state)), //import edilmeliler
+            //we must use stringify and parse methods because we don't want to manipulate the real state object.
+            state : JSON.parse(JSON.stringify(state)),  
             actions,
             getters,
             mutations
@@ -26,41 +27,39 @@ describe("Counter.vue", () => {
     })
     // Exists Check
     describe("exists check", () => {
-        //1. component exist check
+        //1. counter component exist check
         it("should component exists", () => {
             const wrapper = mountComponent()
-            expect(wrapper.exists()).toBeTruthy()
+            expect(wrapper.exists()).toBeTruthy() 
         })
-        //2. decrease button check
+        //2. decrease button exist and text check
         it("should render decrease button", () => {
             const wrapper = mountComponent()
-            const decrease = wrapper.find('#decrease')
-            expect(decrease.exists()).toBeTruthy()
-            expect(decrease.text()).toEqual("Decrease")
+            const decrease = wrapper.find('#decrease') //find the decrease button
+            expect(decrease.exists()).toBeTruthy() //exist
+            expect(decrease.text()).toEqual("Decrease") // button element's should be equal to "Decrease"
         })
-        //3. increase button check
+        //3. increase button exist and text check
         it("should render increase button", () => {
             const wrapper = mountComponent()
-            const increase = wrapper.find('#increase')
-            expect(increase.exists()).toBeTruthy()
-            expect(increase.text()).toEqual("Increase")
+            const increase = wrapper.find('#increase') //find the increase button
+            expect(increase.exists()).toBeTruthy() //exist
+            expect(increase.text()).toEqual("Increase") // button element's should be equal to "Increase"
         })
     })
 
     describe("buttons functionality checks", () => {
         //4. decrease button functionality check
         it("should decrease button work correctly", async () => {
+            //It is the another solution:
             /*const wrapper = mountComponent()
-
             wrapper.find('#decrease').trigger('click')
             await wrapper.vm.$nextTick() //DOM'un update olması beklenir.
-
             let expectedCount = wrapper.vm.$store.state.count 
-
             expect(wrapper.find('span').text()).toEqual(expectedCount + "k")
             console.log(wrapper.find('span').text())*/
 
-            const dispatchMock = jest.fn()
+            const dispatchMock = jest.fn() //mock function
             const wrapper = mount(Counter, {
                 mocks: {
                     $store: {
@@ -69,22 +68,20 @@ describe("Counter.vue", () => {
                     }
                 }
             })
-            wrapper.find('#decrease').trigger('click')
-            expect(dispatchMock).toHaveBeenCalledWith('decrement')
+            wrapper.find('#decrease').trigger('click') //trigger decrease button
+            expect(dispatchMock).toHaveBeenCalledWith('decrement') //Called the decrement function on the Actions object
         })
 
         //5. increase button functionality check
         it("should increase button work correctly", async () => {
+            //It is the another solution:
             /*const wrapper = mountComponent()
-
             wrapper.find('#increase').trigger('click')
-            await wrapper.vm.$nextTick() //DOM'un update olması beklenir.
-
+            await wrapper.vm.$nextTick() //Waits for the DOM to update.
             let expectedCount = wrapper.vm.$store.state.count
-
             expect(wrapper.find('span').text()).toEqual(expectedCount + "k")*/
 
-            const dispatchMock = jest.fn()
+            const dispatchMock = jest.fn() //mock function
             const wrapper = mount(Counter, {
                 mocks: {
                     $store: {
@@ -93,23 +90,23 @@ describe("Counter.vue", () => {
                     }
                 }
             })
-            wrapper.find('#increase').trigger('click')
-            expect(dispatchMock).toHaveBeenCalledWith('increment')
+            wrapper.find('#increase').trigger('click') //trigger the button
+            expect(dispatchMock).toHaveBeenCalledWith('increment') //Called the increment function on the Actions object
         })
 
         //6. 2 increase + 1 decrease functionality check
         it("check increase and decrease functionality together", async() => {
             const wrapper = mountComponent()
 
-            wrapper.find('#increase').trigger('click')
-            wrapper.find('#increase').trigger('click')
-            wrapper.find('#decrease').trigger('click')
-            await wrapper.vm.$nextTick()
+            wrapper.find('#increase').trigger('click') //1.increase
+            wrapper.find('#increase').trigger('click') //2.increase
+            wrapper.find('#decrease').trigger('click') //1.decrease
+            await wrapper.vm.$nextTick() //Waits for the DOM to update.
 
-            let expectedCount = wrapper.vm.$store.state.count
+            let expectedCount = wrapper.vm.$store.state.count //take the count value on the real store.
 
             //expect(expectedCount).toEqual(1);
-            expect(wrapper.find('span').text()).toEqual(expectedCount + "k")
+            expect(wrapper.find('span').text()).toEqual(expectedCount + "k") //span element's text should be equal to "countk"
 
         })
 
